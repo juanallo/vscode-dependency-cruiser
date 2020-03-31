39a8b4e1fb74737199f3b4b0770b173b7136eea1
Juan Manuel Allo Ron <2070972+juanallo@users.noreply.github.com>
2020-03-30 19:45:01 -0700

    adding html to web view

--- src/GraphView.js
+++ src/GraphView.js
@@ -1,12 +1,29 @@
+const buildView = (title, graph) => {
+    return `
+    <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title${title}</title>
+            </head>
+            <body>
+                ${graph}
+            </body>
+        </html>
+        `
+}
+
 module.exports = {
     openGraph({ vscode, fileName, graph }) {
+        const title = `${fileName} Dependencies`
         const panel = vscode.window.createWebviewPanel(
             `dependency-analysis-${Date.now()}`,
-            `${fileName} Dependencies`,
+            title,
             vscode.ViewColumn.One,
             {}
         )
 
-        panel.webview.html = graph
+        panel.webview.html = buildView(title, graph)
     },
 }
